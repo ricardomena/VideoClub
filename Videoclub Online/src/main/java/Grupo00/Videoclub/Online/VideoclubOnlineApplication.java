8c08f8d589054bf9e6efd16cd92eb6a070c284bf
ricardomena <ricardomena07@gmail.com>
2017-05-20 17:48:13 +0200

    Init

---
+++ Videoclub Online/src/main/java/Grupo00/Videoclub/Online/VideoclubOnlineApplication.java
@@ -0,0 +1,12 @@
+package Grupo00.Videoclub.Online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubOnlineApplication.class, args);
+	}
+}
